ea773bb8addb8f0725ca193af08a8e76ab697422
ilya-otroshko <ilya197829@gmail.com>
2019-10-02 14:34:28 +0300

    heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['facebook_key'], ENV['facebook_secret'],{:provider_ignores_state => true}
+    provider :facebook, ENV['facebook_key'], ENV['facebook_secret'],{ token_params: { parse: :json } }
   end
