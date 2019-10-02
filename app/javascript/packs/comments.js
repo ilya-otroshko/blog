997e942359fad8b366db75d0aadfb735240d165c
ilya-otroshko <ilya197829@gmail.com>
2019-10-02 14:53:00 +0300

    heroku

--- app/javascript/packs/comments.js
+++ app/javascript/packs/comments.js
@@ -16,18 +16,8 @@ window.onload = () => {
 });
 
 $(document).bind("fb.loaded", function() {
-    FB.getLoginStatus(function(response) {
-  
-      console.log('FB STATUS: ' + response.status);
-      if(response.status == "connected") {
-  
-        console.log("FB AUTHED");
-  
-        location.href =
-          '/auth/facebook/callback?' +
-          $.param({ signed_request: response.authResponse.signedRequest })
-      }
-        })
+
+            console.log("ok")
         })
 
 $(document).on('click', ".menu__icon", function () {
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
